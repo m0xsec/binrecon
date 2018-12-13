3c4b8c0fcf49cb5873f21f5639a56d1a70f8a4e9
m0x <m0x@m0x.io>
2018-12-12 19:20:02 -0800

    Initial commit, will clean this up later

---
+++ binrecon.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+type paste struct {
+	ScrapeURL string `json:"scrape_url"`
+	FullURL   string `json:"full_url"`
+	Date      string `json:"date"`
+	Key       string `json:"key"`
+	Size      string `json:"size"`
+	Expire    string `json:"expire"`
+	Title     string `json:"title"`
+	Syntax    string `json:"text"`
+	User      string `json:"user"`
+	Data      string `json:"data"`
+}
+
+func pullLatest() []paste {
+	url := "https://scrape.pastebin.com/api_scraping.php?limit=250"
+
+	reconClient := http.Client{
+		Timeout: time.Second * 15, //Maximum of 15 secs
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("User-Agent", "binrecon")
+
+	res, getErr := reconClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	pastes := make([]paste, 0)
+	jsonErr := json.Unmarshal([]byte(body), &pastes)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+	return pastes
+}
+
+func pullPaste(pasteKey string) string {
+	url := "https://scrape.pastebin.com/api_scrape_item.php?i=" + pasteKey
+
+	reconClient := http.Client{
+		Timeout: time.Second * 2, //Maximum of 2 secs
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("User-Agent", "binrecon")
+
+	res, getErr := reconClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	return string(body)
+
+}
+
+//https://gist.github.com/ryanfitz/4191392
+func doEvery(d time.Duration, f func(time.Time)) {
+	for x := range time.Tick(d) {
+		f(x)
+	}
+}
+
+func recon(t time.Time) {
+	file, err := os.OpenFile("pastes/"+"recon-"+t.String()+".json", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0744)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	latest := pullLatest()
+	for i := range latest {
+		latest[i].Data = pullPaste(latest[i].Key)
+		//latest[i].Data = "data here"
+		fmt.Println("[*] Saving paste " + latest[i].Title + " {" + latest[i].Key + "}")
+
+		pasteJSON, _ := json.Marshal(latest[i])
+		fmt.Fprintf(file, string(pasteJSON)+"\n")
+	}
+}
+
+func main() {
+	doEvery(10*time.Minute, recon)
+}
